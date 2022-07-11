b3310d6b0220fc8f281487d05ce4fa84a5214d5d
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-07-11 14:17:05 -0300

    Update consultaCep.js

--- consultaCep.js
+++ consultaCep.js
@@ -8,7 +8,9 @@ const btnEnviar = document.getElementById('enviar');
 const btnNovaConsulta = document.getElementById('novaConsulta');
 const cepDigitado = document.getElementById('cepDigitado');
 
+//Dá fetch na API, recebe o arquivo em .json e preenche os campos(além de desabilitar alguns)
 btnEnviar.onclick = function () {
+	minhaDiv.style.color = 'black';
 	let consultaCep = cepDigitado.value;
 	fetch('https://api.postmon.com.br/v1/cep/' + consultaCep)
 	.then( async (response) => {
@@ -22,12 +24,20 @@ btnEnviar.onclick = function () {
 		btnNovaConsulta.hidden = false;
 	})
 	.catch((err) => {
-	console.log(err);
-	minhaDiv.innerHTML = 'Erro! Alguns CEP\'s não são reconhecidos pelo sistema.<br>Você pode tentar novamente com um CEP diferente.'
+	let msgErro = document.createElement('p');
+	let msgTxt = document.createTextNode('Erro! Alguns CEP\'s não são reconhecidos pelo sistema. Você pode tentar novamente com um CEP diferente.');
+	msgErro.appendChild(msgTxt);
+	minhaDiv.appendChild(msgErro);
 	minhaDiv.style.color = 'red';
+
+	cepDigitado.disabled = true;
+	btnNovaConsulta.hidden = false;
+
+	console.log(err);
 	})
 }
 
+//Habilita novamente os campos para consulta
 btnNovaConsulta.onclick = function () {
 	let minhaDiv = document.querySelectorAll('#minhaDiv p');
 	for(i = 0; i < minhaDiv.length; i++) {
